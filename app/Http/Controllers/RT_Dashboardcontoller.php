956e2920cb0adfb97cfbe153d19a88bcbea01e93
uzidreamcode <77190656+uzidreamcode@users.noreply.github.com>
2024-05-19 16:57:32 +0700

    feat: Update RT Dashboard controller and view

--- app/Http/Controllers/RT_Dashboardcontoller.php
+++ app/Http/Controllers/RT_Dashboardcontoller.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 use App\Models\penduduk;
+use App\Models\Pengumuman_rt;
 use App\Models\User;
 use Alert;
 
@@ -18,27 +19,29 @@ class RT_Dashboardcontoller extends Controller
      * Display a listing of the resource.
      */
     public function index()
-    {
+{
+    $type_menu = 'dashboard';
 
-        $type_menu = 'dashboard';
-
-       
-        $penduduk = Penduduk::where('nik', auth()->user()->nik)->first();
-
-        $password_default = auth()->user()->default_password;
-
-        
-
-        
-
-        if (!$penduduk) {
-            return redirect()->route('error.page')->with('error', 'Data penduduk tidak ditemukan.');
-        }
-        
-        return view('rt.index', compact('type_menu', 'penduduk', 'password_default'));
-        
-        
+    $penduduk = Penduduk::where('nik', auth()->user()->nik)->first();
+    if (!$penduduk) {
+        return redirect()->route('error.page')->with('error', 'Data penduduk tidak ditemukan.');
     }
+
+    $pengumuman_rt = Pengumuman_rt::where('id_rt', $penduduk->id_rt)
+        ->join('pengumuman', 'pengumuman_rt.id_pengumuman', '=', 'pengumuman.id_pengumuman')
+        ->select('pengumuman.*')
+        ->first();
+
+    if ($pengumuman_rt) {
+        $tanggal_pengumuman = $pengumuman_rt->tanggal; 
+    } 
+    $tanggal_sekarang = date('Y-m-d');
+
+    $password_default = auth()->user()->default_password;
+    
+    return view('rt.index', compact('type_menu', 'penduduk', 'password_default', 'pengumuman_rt', 'tanggal_sekarang', 'tanggal_pengumuman'));
+}
+
 
     /**
      * Show the form for creating a new resource.
