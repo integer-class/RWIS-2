bb465482936944b0bc87e941c2ad96c1aaf842a0
mhankazis <buatoslinux@gmail.com>
2024-05-25 21:21:26 +0700

    Refactor form editing logic in RT_KomplainController.php

--- app/Http/Controllers/RT_KomplainController.php
+++ app/Http/Controllers/RT_KomplainController.php
@@ -79,7 +79,8 @@ class RT_KomplainController extends Controller
     }
 
     /**
-     * Show the form for editing the specified resource.
+     * Show the form for editi
+     * ng the specified resource.
      */
     public function edit(string $id)
     {
