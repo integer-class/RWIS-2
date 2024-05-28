f3ab1af703201fb315a0e1ee1a22fdbf27f279f0
definitelynotjin <118191197+definitelynotjin@users.noreply.github.com>
2024-05-28 08:58:50 +0700

    back button (pengumuman)

--- resources/views/rw/data_pengumuman/create.blade.php
+++ resources/views/rw/data_pengumuman/create.blade.php
@@ -27,6 +27,13 @@
             <div class="section-body">
                 {{-- <h2 class="section-title">Users</h2> --}}
 
+                <h2 class="section">
+                    
+                    <a style="width:130px; height:38px; margin-bottom:20px" href="{{ route('pengumuman.index') }}" class="btn btn-lg btn-primary">Kembali</a>
+    
+    
+                </h2>
+
                 @include('sweetalert::alert')
 
 
