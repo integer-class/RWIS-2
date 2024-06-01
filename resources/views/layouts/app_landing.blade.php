25d99523fad1cb349eb35dfc21fe972eee253ede
mhankazis <buatoslinux@gmail.com>
2024-06-01 10:11:51 +0700

    chore: Refactor landing page route to use Landing_indexController

--- resources/views/layouts/app_landing.blade.php
+++ resources/views/layouts/app_landing.blade.php
@@ -1,29 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
 <head>
+    <!-- Meta tags and other head elements -->
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <!-- favicon-->
     <link rel="shortcut icon" href="{{ asset('assets/images/favicon.ico')}}">
-    <!-- Site Title-->
-    <title>RWKU</title>
+    <title>@yield('title')</title>
     <meta name="description" content="A Template for Architectural Interior Design company website.">
-    <!-- Bootstrap CSS file-->
+    <!-- CSS files -->
     <link href="{{ asset('assets/css/bootstrap.min.css')}}" rel="stylesheet"> 
-    <!-- Flickity CSS file-->
     <link href="{{ asset('assets/css/flickity.min.css')}}" rel="stylesheet">
-    <!-- Main CSS file-->
     <link href="{{ asset('assets/css/style.css')}}" rel="stylesheet">
-    <!-- Fontawesome 5 CSS file-->
     <link href="{{ asset('assets/css/fontawesome-all.min.css')}}" rel="stylesheet">
-    <!-- Magnific Popup CSS-->
     <link href="{{ asset('assets/css/magnific-popup.css')}}" rel="stylesheet">
-    <!-- Google Fonts-->
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600&amp;display=swap">
-  </head>
+</head>
+<body>
+    <!-- Navbar -->
+    @include('components.navbar-template')
+    <!-- End Navbar -->
 
-
-  @include('components.navbar-template')
-
-
+    <!-- Main Content -->
     @yield('main')
 
+    <!-- Footer -->
+    @include('components.footer-template')
+    <!-- End Footer -->
+</body>
+
+    <!-- JavaScript files -->
+    <script src="{{ asset('assets/js/jquery.min.js') }}"></script>
+    <script src="{{ asset('assets/js/lozad.min.js') }}"></script>
+    <script src="{{ asset('assets/js/bootstrap.bundle.min.js') }}"></script>
+    <script src="{{ asset('assets/js/aos.js') }}"></script>
+    <script src="{{ asset('assets/js/flickity.pkgd.min.js') }}"></script>
+    <script src="{{ asset('assets/js/jquery.magnific-popup.min.js') }}"></script>
+    <script src="{{ asset('assets/js/jquery.countdown.js') }}"></script>
+    <script src="{{ asset('assets/js/jquery.countTo.js') }}"></script>
+    <script src="{{ asset('assets/js/masonry.pkgd.min.js') }}"></script>
+    <script src="{{ asset('assets/js/global.js') }}"></script>
+    
+</html>
