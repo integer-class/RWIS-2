c2590a42d0c29d2d4a9cf13a4e87d2de5e9bd77b
definitelynotjin <118191197+definitelynotjin@users.noreply.github.com>
2024-06-04 02:37:12 +0700

    little things pt.2

--- resources/views/rw/data_arsip/pengumuman.blade.php
+++ resources/views/rw/data_arsip/pengumuman.blade.php
@@ -69,7 +69,7 @@
                     <div class="col-12">
                         <div class="card">
                             <div class="card-header">
-                                <h4>arsip Warga</h4>
+                                <h4>Arsip Warga</h4>
                             </div>
                             <div class="card-body">
                                 {{-- <div class="float-left">
