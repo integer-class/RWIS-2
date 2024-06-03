9e73819b9e7adf1b80cfec0f3119dcebd64f4fe8
definitelynotjin <118191197+definitelynotjin@users.noreply.github.com>
2024-06-04 02:36:21 +0700

    little things

--- resources/views/rw/data_arsip/index.blade.php
+++ resources/views/rw/data_arsip/index.blade.php
@@ -65,7 +65,7 @@
                     <div class="col-12">
                         <div class="card">
                             <div class="card-header">
-                                <h4>arsip Warga</h4>
+                                <h4>Arsip Warga</h4>
                             </div>
                             <div class="card-body">
                                 {{-- <div class="float-left">
