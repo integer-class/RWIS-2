c54de995e30909c2aee89f688746b2e5cf1e8233
fahrudinibrahim <116633912+FahruddinZaimIbrahim@users.noreply.github.com>
2024-06-04 09:45:11 +0700

    tes

--- resources/views/rw/index.blade.php
+++ resources/views/rw/index.blade.php
@@ -62,7 +62,7 @@
                         </div>
                     </div>
                 </div>
-                </div>  
+       
             </div>
             <div class="row">   
                 <div class="col-lg-8 col-md-12 col-12 col-sm-12">
