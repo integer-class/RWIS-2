c1855f88aeb496cd5ef2762208d5e97ddb27c0fc
definitelynotjin <118191197+definitelynotjin@users.noreply.github.com>
2024-06-05 17:18:09 +0700

    profile

--- app/Http/Controllers/Warga_DashboardController.php
+++ app/Http/Controllers/Warga_DashboardController.php
@@ -73,21 +73,28 @@ class Warga_DashboardController extends Controller
     /**
      * Display the specified resource.
      */
-    public function show(Penduduk $penduduk)
+
+     //show ver 1
+    public function show($id)
     {
-        dd($penduduk);
-        // $penduduk = \App\Models\Penduduk::join('users', 'penduduk.nik', '=', 'users.nik')
-        //     ->join('rt', 'users.id_rt', '=', 'rt.id_rt')
-        //     ->where('penduduk.nik', $penduduk->nik)
-        //     ->first();
-        // $jumlah_anggota_keluarga = \App\Models\Penduduk::where('nomor_kk', $penduduk->nomor_kk)->count();
-        // $komplain = \App\Models\Komplain::where('nik', $penduduk->nik)->count();
-        // $penduduk_kk = \App\Models\Penduduk::where('nomor_kk', $penduduk->nomor_kk)
-        //     ->where('nik', '!=', $penduduk->nik) // Exclude the person
-        //     ->get();
-        // $type_menu = 'detail_penduduk'; 
-        // return view('warga.show', compact('penduduk', 'type_menu', 'penduduk_kk', 'jumlah_anggota_keluarga', 'komplain'));
+
+        echo $id;
+        $penduduk = \App\Models\Penduduk::join('users', 'penduduk.nik', '=', 'users.nik')
+            ->join('rt', 'users.id_rt', '=', 'rt.id_rt')
+            ->where('penduduk.nik', $id)
+            ->first();
+
+          
+        $jumlah_anggota_keluarga = \App\Models\Penduduk::where('nomor_kk', $penduduk->nomor_kk)->count();
+        $komplain = \App\Models\Komplain::where('nik', $penduduk->nik)->count();
+        $penduduk_kk = \App\Models\Penduduk::where('nomor_kk', $penduduk->nomor_kk)
+            ->where('nik', '!=', $penduduk->nik) 
+            ->get();
+        $type_menu = 'detail_penduduk'; 
+        return view('warga.show', compact('penduduk', 'type_menu', 'penduduk_kk', 'jumlah_anggota_keluarga', 'komplain'));
     }
+
+
 
     /**
      * Show the form for editing the specified resource.
--- app/Models/Penduduk.php
+++ app/Models/Penduduk.php
@@ -11,6 +11,8 @@ class Penduduk extends Model
 
     protected $table = 'penduduk';
     protected $primaryKey = 'nik';
+    public $incrementing = false; // Because nik is not an auto-incrementing integer
+    protected $keyType = 'string'; // Assuming nik is a string, set this to 'int' if it's an intege
 
 
 
@@ -37,7 +39,7 @@ class Penduduk extends Model
 
     public function KartuKeluarga()
     {
-        return $this->belongsTo(KartuKeluarga::class);
+        return $this->belongsTo(KartuKeluarga::class, 'nomor_kk', 'nomor_kk');
     }
 
     public function rt()
